226715027605bd77131c1b3c9a33e05f7ae6534b
YulliaBerezniak <fedorova2097@gmail.com>
2024-09-07 22:54:26 +0300

    PasswordCheck

---
+++ PasswordCheck.js
@@ -0,0 +1,29 @@
+function PasswordCheck(Password)  {
+    let haslowerCase = false;
+    let hasupperCase = false;
+    let hasDigit = false;
+    for (let i = 0; i < Password.length; i++) {
+      const char = Password[i];
+      if (char >= '0' && char <= '9') {
+        hasDigit = true;
+      } 
+      else if (char === char.tolowerCase() && char !== char.toupperCase()) {
+        haslowerCase = true;
+      } 
+      else if (char === char.toupperCase() && char !== char.tolowerCase()) {
+        hasupperCase = true;
+      }
+      if (haslowerCase && hasupperCase && hasDigit) {
+        break;
+      }
+    }
+    if (password.length >= 8 && haslowerCase && hasupperCase && hasDigit) {
+      return "Strong";
+    } else {
+      return "Weak";
+    }
+  }
+
+PasswordCheck(Yuliia1234)
+
+  
